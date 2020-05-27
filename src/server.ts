0eee18c2f4e4b525d102d338cd0637af9fbf475c
Ekansh Jain <ejekanshjain@gmail.com>
2020-05-27 20:23:30 +0530

    fix cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { config } from 'dotenv'
 import express, { Application } from 'express'
+import cors from 'cors'
 
 config()
 
@@ -9,6 +10,7 @@ const port: number = parseInt(process.env.PORT ?? '5000')
 
 const app: Application = express()
 
+app.use(cors)
 app.use(express.json())
 
 app.use('/', router)
